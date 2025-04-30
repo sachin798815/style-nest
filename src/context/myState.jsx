89f702913b0d02e7e38561988b0eb60b34e340e1
sachin798815 <sachin798815@gmail.com>
2025-04-30 23:21:03 +0530

    admin add product and get product working for admin dashboard

--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,11 +1,50 @@
-import { useState } from "react";
-import MyContext from "./myContext";
+import { useEffect, useState } from 'react';
+import MyContext from './myContext';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { fireDB } from '../firebase/firebaseConfig';
 
 function MyState({ children }) {
-  const [loading,setLoading]=useState(false);
-  return <MyContext.Provider value={{
-    loading,setLoading
-  }}>{children}</MyContext.Provider>;
+    // Loading State 
+    const [loading, setLoading] = useState(false);
+
+    // User State
+    const [getAllProduct, setGetAllProduct] = useState([]);
+
+    const getAllProductFunction = async () => {
+        setLoading(true);
+        try {
+            const q = query(
+                collection(fireDB, "products"),
+                orderBy('time')
+                //orders data according to time
+            );
+            const data = onSnapshot(q, (QuerySnapshot) => {
+                let productArray = [];
+                QuerySnapshot.forEach((doc) => {
+                    productArray.push({ ...doc.data(), id: doc.id });
+                });
+                setGetAllProduct(productArray);
+                setLoading(false);
+            });
+            return () => data;
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getAllProductFunction();
+    }, []);
+    return (
+        <MyContext.Provider value={{
+            loading,
+            setLoading,
+            getAllProduct
+        }}>
+            {children}
+        </MyContext.Provider>
+    )
 }
 
-export default MyState;
+export default MyState
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import OrderDetail from '../../components/admin/OrderDetail';
 import UserDetail from '../../components/admin/userDetail';
 
 const AdminDashboard = () => {
+    const user = JSON.parse(localStorage.getItem('users'));
     return (
         <div>
             {/* Top */}
@@ -24,8 +25,18 @@ const AdminDashboard = () => {
                         </div>
                         {/* text  */}
                         <div className="">
-                            <h1 className=" text-center text-lg text-violet-500"><span className=" font-bold">Name :</span> Sachin Kumar</h1>
-                            <h1 className=" text-center text-lg text-violet-500"><span className=" font-bold">Email :</span> test1@gmail.com</h1>
+                            <h1 className=" text-center text-lg text-violet-500"><span className=" font-bold">Name :</span>{user?.name}</h1>
+                            <h1 className=" text-center text-lg text-violet-500"><span className=" font-bold">Email :</span>{user?.email}</h1>
+                            {/* Date  */}
+                            <h1 className=" text-center text-lg text-pink-500">
+                                <span className=" font-bold">Date : </span>
+                                {user?.date}
+                            </h1>
+                            {/* Role  */}
+                            <h1 className=" text-center text-lg text-pink-500">
+                                <span className=" font-bold">Role : </span>
+                                {user?.role}
+                            </h1>
                         </div>
                     </div>
                 </div>
