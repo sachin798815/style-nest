594182b1098018f12ae90363dba570891d98ea10
sachin798815 <sachin798815@gmail.com>
2025-05-01 00:15:00 +0530

    fetched list on homepage from db

--- src/components/homePageProductCard/HomePageProductCard.jsx
+++ src/components/homePageProductCard/HomePageProductCard.jsx
@@ -1,84 +1,13 @@
-import { useNavigate } from "react-router-dom";
-
-// productData 
-const productData = [
-    {
-      id: 1,
-      image: 'https://images.pexels.com/photos/2635315/pexels-photo-2635315.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Black, White Red Stripes',
-      desc: 'A classic black, white, and red striped shirt that blends contemporary style with a bold look, ideal for casual outings.',
-      price: 8200,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 2,
-      image: 'https://images.pexels.com/photos/3751397/pexels-photo-3751397.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Dark Green T-Shirt',
-      desc: 'A dark green t-shirt featuring minimalistic design for a relaxed and stylish look, perfect for any casual event.',
-      price: 11375,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 3,
-      image: 'https://images.pexels.com/photos/1188748/pexels-photo-1188748.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Regrets T-Shirt',
-      desc: 'A thought-provoking t-shirt with an artistic print, symbolizing the concept of regrets, a must-have for creative minds.',
-      price: 2400,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 4,
-      image: 'https://images.pexels.com/photos/10663488/pexels-photo-10663488.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Colorful Printed Shirt',
-      desc: 'A vibrant and colorful shirt adorned with intricate hand-painted patterns, offering a bold yet sophisticated look.',
-      price: 13500,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 5,
-      image: 'https://images.pexels.com/photos/8638712/pexels-photo-8638712.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Flower Printed Shirt',
-      desc: 'A beautiful flower printed shirt in premium cotton, combining comfort with elegance, perfect for a laid-back summer vibe.',
-      price: 7000,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 6,
-      image: 'https://images.pexels.com/photos/18563162/pexels-photo-18563162/free-photo-of-a-man-in-a-tan-shirt-sitting-on-a-chair.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Misunderstood T-Shirt',
-      desc: 'A unique blue shirt with hand-painted designs that make a statement about individuality and the misunderstood concepts of life.',
-      price: 2999,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 7,
-      image: 'https://images.pexels.com/photos/31826364/pexels-photo-31826364/free-photo-of-fashionable-woman-posing-near-suv-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Puma Black T-Shirt',
-      desc: 'A premium black t-shirt from Puma with a hand-painted depiction of Lord Hanuman, combining culture with modern style.',
-      price: 3999,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-    {
-      id: 8,
-      image: 'https://images.pexels.com/photos/17243499/pexels-photo-17243499/free-photo-of-a-woman-leaning-against-a-wall-wearing-a-crop-top.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Diamond Half T-Shirt',
-      desc: 'A stylish light blue cotton shirt with abstract hand-painted motifs, offering a modern twist on traditional artwork.',
-      price: 11700,
-      trendingProductName: 'Featured',
-      quantity: 1,
-    },
-];
-   
+import { useContext } from "react";
+import { useNavigate } from "react-router";
+import myContext from "../../context/myContext";
+import Loader from "../loader/Loader";
 
 const HomePageProductCard = () => {
     const navigate = useNavigate();
+
+    const context = useContext(myContext);
+    const {loading, getAllProduct} = context;
     return (
         <div className="mt-10">
             {/* Heading  */}
@@ -89,18 +18,21 @@ const HomePageProductCard = () => {
             {/* main  */}
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-5 mx-auto">
+                    <div className="flex justify-center">
+                    {loading&&<Loader/>}
+                    </div>
                     <div className="flex flex-wrap -m-4">
-                        {productData.map((item, index) => {
-                            const { image, title, price } = item
+                        {getAllProduct.slice(0,8).map((item, index) => {
+                            const { id, title, price,productImageUrl } = item
                             return (
                                 <div key={index} className="p-4 w-full md:w-1/4">
                                     <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
-                                            <img
-                                                className="lg:h-80  h-96 w-full object-cover"
-                                                src={image}
-                                                alt="blog"
-                                                onClick={()=>navigate('/productinfo')}
-                                            />
+                                        <img
+                                        onClick={()=> navigate('/productinfo')}
+                                            className="lg:h-80  h-96 w-full object-cover"
+                                            src={productImageUrl}
+                                            alt="blog"
+                                        />
                                         <div className="p-6">
                                             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                                                 Trending
@@ -113,7 +45,7 @@ const HomePageProductCard = () => {
                                             </h1>
 
                                             <div className="flex justify-center ">
-                                                <button className=" bg-rose-500 hover:bg-rose-400 w-full text-white py-[4px] rounded-lg font-bold">
+                                                <button className=" bg-violet-500 hover:bg-violet-600 w-full text-white py-[4px] rounded-lg font-bold">
                                                     Add To Cart
                                                 </button>
                                             </div>
