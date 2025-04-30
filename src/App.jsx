fd7bebfcb2b1cd3d08755d39331d8124d740d38b
sachin798815 <sachin798815@gmail.com>
2025-04-30 18:16:42 +0530

    dummy cart created

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ScrollTop from "./components/scrollTop/ScrollTop";
 import ProductInfo from "./pages/productInfo/ProductInfo";
+import CartPage from "./pages/cart/CartPage";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/" element={<HomePage/>}/>
           <Route path="/*" element={<NoPage/>}/>
           <Route path="/productinfo" element={<ProductInfo/>}/>
+          <Route path="/cart" element={<CartPage/>}/>
 
         </Routes>
       </Router>
